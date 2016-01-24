7ffba6ad95c6b158bb8a789301fc123c71173890
Andres Gutierrez <andres@redbooth.com>
2016-01-24 16:08:34 +0100

    SVG app component logo

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 import styles from './styles/index.css';
+import Logo from '../Logo';
 
 export class App extends Component {
   render() {
     return (
       <div className={styles.app}>
+        <Logo/>
         App base component
       </div>
     );
